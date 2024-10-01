debd153c6b22b98ce6bc0a646b28b665abd96ff9
Suhyun_Kim <sue.99kim@gmail.com>
2024-10-01 20:44:38 +1000

    Add panel files

--- Milestone2_PythonProject/PanelBreakdown.py
+++ Milestone2_PythonProject/PanelBreakdown.py
@@ -1,4 +1,4 @@
+#from template_breakdown_panel import BreakdownPanel
 from template_main_panel import MainPanel
-
-class PanelBreakdown(MainPanel):
+class PanelBreakdown(MainPanel): #Change this argument to the class name of template_breakdown_panel.py
     pass
--- Milestone2_PythonProject/PanelRangeFilter.py
+++ Milestone2_PythonProject/PanelRangeFilter.py
@@ -1,4 +1,4 @@
+#from template_range_filter_panel import RangeFilterPanel
 from template_main_panel import MainPanel
-
-class PanelRangeFilter(MainPanel):
+class PanelRangeFilter(MainPanel) : #Change this argument to the class name of template_range_filter_panel.py'''):
     pass
