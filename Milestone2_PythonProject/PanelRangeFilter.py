f9501f9b0297b41d8f27f7d82b8e6655a5b1b43a
Suhyun_Kim <sue.99kim@gmail.com>
2024-10-02 12:54:15 +1000

    update

--- Milestone2_PythonProject/PanelRangeFilter.py
+++ Milestone2_PythonProject/PanelRangeFilter.py
@@ -1,4 +1,4 @@
-#from template_range_filter_panel import RangeFilterPanel
-from template_main_panel import MainPanel
-class PanelRangeFilter(MainPanel) : #Change this argument to the class name of template_range_filter_panel.py'''):
+from template_range_filter_panel import NutritionRangeFilterPanel
+
+class PanelRangeFilter(NutritionRangeFilterPanel) : #Change this argument to the class name of template_range_filter_panel.py'''):
     pass
